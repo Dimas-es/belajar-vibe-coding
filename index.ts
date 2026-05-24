abbcf16271656e0eca3e62897228eab4b8801b13
Dimas-es <evosdimm@gmail.com>
2026-05-24 13:44:16 +0700

    feat: implementasi login user dan manajemen session dengan Drizzle

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia";
 import { db } from "./src/db";
 import { userRoutes } from "./src/routes/user-route";
+import { authRoutes } from "./src/routes/auth-route";
 
 const app = new Elysia()
   // Integrasikan koneksi Drizzle ke dalam context Elysia
@@ -17,6 +18,9 @@ const app = new Elysia()
   
   // Daftarkan rute manajemen user (termasuk POST /api/users)
   .use(userRoutes)
+  
+  // Daftarkan rute autentikasi (termasuk POST /api/login)
+  .use(authRoutes)
   
   .listen(3000);
 
